6f855164e9d482b09b332df98838a828451f2d1e
mayados <mariannedos.cours@gmail.com>
2022-11-15 13:25:55 +0100

    create new objects

--- index.php
+++ index.php
@@ -18,22 +18,39 @@
             include $class_name . '.php';
         });
 
+        /* PAYS */
+
         $portugal = new Pays("Portugal");
         $france = new Pays("France");
 
+        /* JOUEUR */
         $ronaldo = new Joueur("Cristiano","Ronaldo","12-03-1985", $portugal);
         $mbappe = new Joueur("Kylian","Mbappé","27-08-1992", $france);
+        $messi = new Joueur("Lionel","Messi","14-10-1989",$france);
         //  echo $mbappe;
+
+        /* EQUIPE */
         /* On crée un nouvel objet Equipe */
         $psg = new Equipe("PSG", $france);
         $om = new Equipe("OM",$france);
         $racingClub = new Equipe("Racing Club Strasbourg",$france);
+        $porto = new Equipe("FC Porto",$portugal);
         /* On affiche les équipes pour le pays voulu */
-        echo $france->afficherEquipes();
 
+
+        /* CARRIERE */
         $psgMbappe = new Carriere($mbappe,$psg,2022);
         $omMbappe = new Carriere($mbappe,$om,2020);
+        $portoMbappe = new Carriere($mbappe,$porto,2018);
+        $psgMessi = new Carriere($messi,$psg,2015);
+        $portoRonaldo = new Carriere($ronaldo,$porto,2022);
+
+        /* CALL TO FUNCTIONS */
+        echo $france->afficherEquipes();
+
+        echo $portugal->afficherEquipes();
         echo $psg->afficherJoueurs();
+        echo $porto->afficherJoueurs();        
         echo $mbappe->afficherCarrieres();
 
 
