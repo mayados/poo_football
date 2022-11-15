371e397d36a69c9c6b5657ffc7f1ed9a3d055e6d
mayados <mariannedos.cours@gmail.com>
2022-11-15 15:51:23 +0100

    add usort function for the dates

--- Joueur.php
+++ Joueur.php
@@ -57,6 +57,17 @@
         /* Une fonction d'affichage pour afficher les équipes d'un certain pays */
         public function afficherCarrieres() {
             echo "<br>Les carrières de $this->firstName $this->lastName : <br>";
+
+            /* On trie le tableau $this->joueurs de par une fonction anonyme qui prend en paramètres une variable a et une variable b */
+            usort ($this->carrieres,function ($a,$b)
+            {
+                /* On compare les 2 années qu'on récupère grâce à la fonction getAnnée contenue dans Joueur */
+                return ($b->getAnnee()) <=> ($a->getAnnee());
+  
+            }    
+            )  
+            ;
+
             /* Pour chaque élément de l'array $this->equipes, on affiche l'équipe */
             foreach($this->carrieres as $carriere) {
                 echo $carriere ."<br>";
