039924afc29f8869cfa89894e0373cb5e4544b8f
mayados <mariannedos.cours@gmail.com>
2022-11-15 11:51:43 +0100

    link to objects functions

--- Carriere.php
+++ Carriere.php
@@ -4,5 +4,21 @@
         private Equipe $equipe;
         private int $annee;
 
+        public function __construct(Joueur $joueur, Equipe $equipe, int $annee)
+        {
+            $this->joueur = $joueur;
+            $this->joueur->ajouterCarriere($this);
+            $this->equipe = $equipe;
+            /* On ajoute l'objet joueur entré en paramètres en tant que paramètre de fonction ajouterJoueur de la class Equipe entrée en paramètres = ça ajoute automatiquement le joueur */
+            $this->equipe->ajouterJoueur($this->joueur);
+            $this->annee = $annee;
+        }
+
+
+        public function __toString()
+        {
+            return " $this->equipe $this->annee";
+        }
+
     }
 ?>
