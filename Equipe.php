9273566eabdb73cfe0fa896481ea1945d45c8dc3
mayados <mariannedos.cours@gmail.com>
2022-11-15 11:52:27 +0100

    add function ajouterJoueur and afficherJoueurs

--- Equipe.php
+++ Equipe.php
@@ -12,7 +12,7 @@
             /* On indique que les propriétés de l'objet sont liées aux variables passées en paramètres */
             $this->name = $name;
             $this->pays = $pays;
-            /* On ajoute l'équipe qu'on crée dans la méthode ajouterEquipe() du pays, pour que ca ajoute automatiquement les équipes par pays (au lieu de devoir appeler  les functions manuellement dans index) */
+            /* On ajoute l'équipe qu'on crée dans la méthode ajouterEquipe() du pays, pour que ca ajoute automatiquement les équipes par pays (au lieu de devoir appeler  les functions manuellement dans index) = à chaque fois qu'on crée un nouvel objet Equipe, ça ajoute automatiquement l'équipe au pays concerné */
             $this->pays->ajouterEquipe($this);
             /* De base, joueurs est un array vide, car on ajoute au fur et à mesure */
             $this->joueurs = [];
@@ -31,15 +31,31 @@
 
         /* Pouor pouvoir ajouter plusieurs objets joueurs au club */
         /* Lorsqu'il y a ..., cela signifie que l'on peut en mettre plusieurs, ca se comporte comme un tableau */
-        public function setJoueurs(Joueur ...$joueurs)
+        // public function setJoueurs(Joueur ...$joueurs)
+        // {
+        //     /* Pour chaque joueur.. */
+        //     foreach($joueurs as $joueur)
+        //     {
+        //         /* On ajoute chaque joueur dans le tableau $joueurs de notre class Equipe */
+        //         array_push($this->joueurs,$joueur);
+        //     }
+        //      echo "Joueurs du club $this->name : <br>".implode("<br>",$this->joueurs);                           
+        // }
+
+        public function ajouterJoueur(Joueur $joueur)
         {
-            /* Pour chaque joueur.. */
-            foreach($joueurs as $joueur)
-            {
-                /* On ajoute chaque joueur dans le tableau $joueurs de notre class Equipe */
-                array_push($this->joueurs,$joueur);
+            // array_push($this->equipes,$equipe);
+            /* Il est mieux d'utiliser cette syntaxe, plus moderne au lieu de array_push */
+            $this->joueurs[] = $joueur;
+        }
+
+        /* Une fonction d'affichage pour afficher les équipes d'un certain pays */
+        public function afficherJoueurs() {
+            echo "<br>Les joueurs de l'équipe $this->name : <br>";
+            /* Pour chaque élément de l'array $this->equipes, on affiche l'équipe */
+            foreach($this->joueurs as $joueur) {
+                echo $joueur ."<br>";
             }
-             echo "Joueurs du club $this->name : <br>".implode("<br>",$this->joueurs);                           
         }
 
 
